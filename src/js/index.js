c2c2404d7be48afa4ccbd6d6e6d8087e63829381
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2018-07-22 17:48:23 +0200

    Added code of the third video.

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,28 @@ console.log("inizio");
 
 const basicScene = new BasicScene();
 basicScene.InitScene();
+const camera = basicScene.camera;
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const plane = new THREE.Plane();
+const planeNormal = new THREE.Vector3();
+const point = new THREE.Vector3();
+const distanceFromCamera = 50;
 
 window.document.addEventListener("click", (e) => {
+  mouse.x = (e.clientX / window.innerWidth) * 2 - 1; // 0-1 -> -1-1
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  planeNormal.copy(camera.position).normalize();
+  plane.set(planeNormal, distanceFromCamera);
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, point);
+
   const fadingCube = new FadingCube();
-  fadingCube.life = 35;
+  fadingCube.life = 5;
   fadingCube.initialTime = basicScene.clock.getElapsedTime();
+  fadingCube.root.position.copy(point);
+
   console.log(fadingCube.initialTime);
   basicScene.Add(fadingCube);
 });
