c7d7ec0a2eacbe5e4c0c22e4166e34c4a0bf5974
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2019-01-26 18:05:07 +0100

    added folders back

---
+++ src/js/Loops/CubeLoop.js
@@ -0,0 +1,143 @@
+import * as THREE from 'three';
+import Lights from '../Lights/BlueParty.js';
+import BasicCube from '../Objects/BasicCube.js';
+
+export default class CubeLoop {
+  constructor({scene, onLoad}) {
+    const clearColor = new THREE.Color();
+    clearColor.setIntHSL(48, 90, 64);
+    scene.renderer.setClearColor(clearColor);
+
+    const frustumSize = 10;
+    const aspect = scene.camera.aspect;
+
+    scene.camera = new THREE.OrthographicCamera(
+        frustumSize * aspect / - 2,
+        frustumSize * aspect / 2,
+        frustumSize / 2,
+        frustumSize / - 2,
+        1,
+        150
+    );
+    scene.camera.position.copy(new THREE.Vector3(-10, 10, 10));
+    scene.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    this.root = new THREE.Group();
+
+    const lights = new Lights();
+    this.root.add(lights);
+
+		const cubeMat = new THREE.MeshStandardMaterial({
+			color: 0xffffff,
+			metalness: 0.4,
+			roughness: 0.2,
+		});
+
+    this.cubes = [
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(-1, 1, 1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(1, 1, 1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(-1, 1, -1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(1, 1, -1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(-1, -1, 1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(1, -1, 1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(-1, -1, -1)
+      }),
+      new BasicCube({
+        size: 2,
+        material: cubeMat,
+        position: new THREE.Vector3(1, -1, -1)
+      })];
+
+    this.cubesParent = new THREE.Group();
+    for (let cube of this.cubes) {
+      this.cubesParent.add(cube.root);
+    }
+    this.cubesParent.position.copy(new THREE.Vector3(1, -1, -1));
+    this.root.add(this.cubesParent);
+
+    if (onLoad) onLoad();
+  }
+
+  update(timeElapsed) {
+    const animationTime = timeElapsed * 3;
+    const loopTime = animationTime % (Math.PI * 2);
+
+    if (loopTime < Math.PI) {
+      this.firstSection(animationTime);
+    } else {
+      this.secondSection(animationTime);
+    }
+  }
+
+  firstSection(animationTime) {
+    const sinTime = Math.sin(animationTime * 0.5);
+    const absSinTime = Math.abs(sinTime);
+
+    this.cubesParent
+      .children[7]
+      .position
+      .copy(new THREE.Vector3(1, -1, -1));
+    
+    for (let i = 0; i < this.cubesParent.children.length; i++) {
+      const child = this.cubesParent.children[i];
+      const rota = (Math.clamp(((1 - absSinTime) * (Math.PI * 0.5) + (i * 0.125)), 0, 1));
+      const sinScale = new THREE.Vector3(rota, rota, rota);
+      child.scale.copy(sinScale);
+      child.rotation.z = (rota * Math.PI);
+      child.rotation.x = rota * Math.PI * 0.5;
+    }
+  }
+
+  secondSection(animationTime) {
+    const sinTime = Math.sin(animationTime * 0.5);
+    const absSinTime = Math.abs(sinTime);
+
+    const cosTime = Math.cos(animationTime * 0.5);
+    const absCosTime = Math.abs(cosTime);
+
+    const child = this.cubesParent.children[7];
+    const rota = (Math.clamp(((1 - absSinTime) * (Math.PI * 0.5) + (7 * 0.125)) + (absCosTime), 0, 2));
+    const sinScale = new THREE.Vector3(rota, rota, rota);
+    child.scale.copy(sinScale);
+    child.rotation.z = (rota * Math.PI);
+    child.rotation.x = rota * Math.PI * 0.5;
+    const oldPos = new THREE.Vector3(1, -1, -1);
+    child.position.copy(oldPos.lerp(new THREE.Vector3(0, 0, 0), Math.clamp((1 - absSinTime) * 6, 0, 1)));
+    for (let i = 0; i < this.cubesParent.children.length - 1; i++) {
+      const child = this.cubesParent.children[i];
+      const rota = Math.clamp(((Math.clamp(((1 - absSinTime) * (Math.PI * 0.5) + (i * 0.125)), 0, 1)) -
+        absCosTime), 0, 1);
+      const sinScale = new THREE.Vector3(rota, rota, rota);
+      child.scale.copy(sinScale);
+      child.rotation.z = (rota * Math.PI);
+      child.rotation.x = rota * Math.PI * 0.5;
+    }
+  }
+}
---
+++ src/js/Loops/FloatingBio.js
@@ -0,0 +1,121 @@
+import * as THREE from 'three';
+import Lights from '../Lights/MagentaParty.js';
+import BasicCylinder from '../Objects/BasicCylinder.js';
+
+export default class FloatingBio {
+    constructor({scene, onLoad}) {
+        this.scene = scene;
+        
+        scene.camera.far = 350;
+        this.scene.camera.updateProjectionMatrix();
+            
+        const clearColor = new THREE.Color();
+        clearColor.setIntHSL(48, 90, 64);
+        scene.renderer.setClearColor(clearColor);
+        scene.scene.fog = new THREE.Fog(clearColor, 0.1, 300);
+
+        this.root = new THREE.Group();
+
+        const lights = new Lights();
+        this.root.add(lights);
+
+        const cubeMat = new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            metalness: 0.4,
+            roughness: 0.2,
+        });
+
+        this.amoebas = [];
+        const slices = 17;
+        const amebaSize = 15;
+        const amebaCount = 60;
+
+        for (let i = -amebaCount; i < amebaCount; i++) {
+            const newAmeba = this.newAmeba({
+                slices,
+                amebaSize,
+                material: cubeMat,
+                xPosition: i*amebaSize*0.1
+            });
+
+            this.amoebas.push(newAmeba);
+            this.root.add(newAmeba);
+        }
+
+        if (onLoad) onLoad();
+    }
+    
+
+    newAmeba(params) {
+        const {slices, amebaSize, material, xPosition = 0} = params;
+
+        const piK = Math.PI / slices * 0.5;
+        const ameba = new THREE.Group();
+        const zDistribution = -(this.scene.camera.far - amebaSize);
+
+        ameba.translateZ(Math.random() * zDistribution);
+        for (let y = -slices; y < slices; y++) {
+            const scaleX = Math.cos(piK * y) * amebaSize;
+            const scaleY = Math.sin(piK * y * 0.7) * amebaSize;
+            const newCube = new BasicCylinder({
+                size: 1,
+                material: material,
+                position: new THREE.Vector3(
+                    xPosition * (ameba.position.z / (amebaSize * 3)),
+                    scaleY,
+                    0
+                )
+            });
+            newCube.root.scale.copy(new THREE.Vector3(
+                scaleX * 0.7,
+                piK * 0.6 * scaleX,
+                scaleX * 0.7
+            ));
+            ameba.add(newCube.root);
+        }
+        ameba.userData = {
+            'initialPosition': ameba.position.clone(),
+            'randomValue': Math.random()
+        };
+        return ameba;
+    }
+
+    update(timeElapsed) {
+        const animationTime = timeElapsed * 3;
+        
+        for (let a = 0; a < this.amoebas.length; a++) {
+            this.updateAmeba(a, animationTime);
+        }
+
+        this.updateCamera(timeElapsed * .5);
+    }
+
+    updateAmeba(a, animationTime) {
+        const ameba = this.amoebas[a];
+        for (let i = 0; i < ameba.children.length; i++) {
+            const child = ameba.children[i];
+            const newRot = child.position;
+            child.position.copy(newRot);
+            child.translateY(Math.sin(animationTime + (i * 0.08 + ameba.userData.randomValue) + (a * Math.PI)) * 0.12);
+            child.rotateY(0.005 + (i * 0.001));
+        }
+        const newPos = ameba.userData.initialPosition.clone();
+        const sawTime = (animationTime * 0.01 + (a * Math.PI)) * Math.pow(Math.abs(ameba.position.z * 0.01), 0.3);
+        const sawAmp = 250;
+        newPos.y = (2 * (sawTime - Math.floor(0.5 + sawTime))) * sawAmp;
+        ameba.position.copy(newPos);
+    }
+
+    updateCamera(timeElapsed) {
+        this.scene.camera.position.copy(
+            new THREE.Vector3(
+                0,
+                (Math.cos(timeElapsed) * 14),
+                10)
+            );
+        this.scene.camera.lookAt(new THREE.Vector3(0, 0, -60));
+        
+        this.scene.camera.updateProjectionMatrix();
+    }
+
+}
