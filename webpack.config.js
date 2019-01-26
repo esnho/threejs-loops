b5b77a2d00fd1c68010a65ae39aa6b0debe40d6f
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2019-01-26 12:47:27 +0100

    updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
               loader: 'babel-loader',
               options: {
                 presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-syntax-dynamic-import']
+                plugins: ["dynamic-import-node"]
               }
             }
           },
