b5709743abffc04b4f85a67e63b0450db24d2275
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2019-02-11 22:52:07 +0100

    Added obj to source control

--- webpack.config.js
+++ webpack.config.js
@@ -38,13 +38,13 @@ module.exports = {
           },
           {
             test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-              use: [{
-                  loader: 'file-loader',
-                  options: {
-                      name: '[name].[ext]',
-                      outputPath: 'fonts/'
-                  }
-              }]
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'
+                }
+            }]
           },
           {
             test: /\.(glsl|vs|fs|vert|frag)$/,
@@ -56,7 +56,13 @@ module.exports = {
           },
           {
             test: /\.obj$/,
-            loader: 'file-loader'
+            use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'obj/'
+              }
+            }]
           }
         ]
     },
